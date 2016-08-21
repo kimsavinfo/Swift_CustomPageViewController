7fa7ccfe2bc3a60680bbb82f8a5a8246b04180ea
Kim SAVAROCHE <kimsavinfo@gmail.com>
2016-08-21 15:51:34 +0200

    init

---
+++ CustomPageViewController/CustomPageViewController/TipsViewController.swift
@@ -0,0 +1,26 @@
+//
+//  TipsViewController.swift
+//  CustomPageViewController
+//
+//  Created by Kim SAVAROCHE on 21/08/2016.
+//  Copyright © 2016 Kim SAVAROCHE. All rights reserved.
+//
+
+import UIKit
+
+class TipsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
+
