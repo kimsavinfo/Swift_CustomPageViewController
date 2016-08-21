f420a9a8a2969b9b8ed14e7a91a9d1f29f0b9689
Kim SAVAROCHE <kimsavinfo@gmail.com>
2016-08-21 20:12:08 +0200

    Set Colour

--- CustomPageViewController/CustomPageViewController/TipsRootViewController.swift
+++ CustomPageViewController/CustomPageViewController/TipsRootViewController.swift
@@ -10,10 +10,18 @@ import UIKit
 
 class TipsRootViewController: UIViewController {
     
-    var pageViewController: UIPageViewController?
+    @IBOutlet var pageControl: UIPageControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        changePageControlColour()
+    }
+    
+    func changePageControlColour() {
+        pageControl.currentPageIndicatorTintColor = UIColor(red:0.40, green:0.42, blue:0.46, alpha:1.0)
+        pageControl.pageIndicatorTintColor = UIColor(red:0.72, green:0.72, blue:0.84, alpha:1.0)
+
     }
     
     override func didReceiveMemoryWarning() {
